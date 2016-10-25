60282557db0fc2b09f187a3d2cf62743448c73c7
Stephen Pendergrass <stephendpendergrass@gmail.com>
2016-10-25 00:23:52 -0500

    index text change and lat added to js file for maps

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -427,7 +427,7 @@
 
                 // Create a map object, and include the MapTypeId to add
                 // to the map type control.
-                var $latlng = new google.maps.LatLng(52.5075419, 13.4261419),
+                var $latlng = new google.maps.LatLng(36.312039, -86.598704),
                     $mapOptions = {
                         zoom: 13,
                         center: $latlng,
